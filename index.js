701b47d472b429cb636e99cca10c33196a2f2add
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-11-30 18:33:47 +0600

    admin get api added

--- index.js
+++ index.js
@@ -18,11 +18,29 @@ const client = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1,
 });
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "invalid token" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
 async function run() {
     try {
         const categoriesCollection = client
             .db("SecondSet")
             .collection("categories");
+        const usersCollection = client.db("SecondSet").collection("users");
 
         // get all categories
         app.get("/categories", async (req, res) => {
@@ -37,6 +55,41 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const category = await categoriesCollection.findOne(query);
             res.send(category);
+        });
+
+        // get access token
+        app.get("/jwt", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign(
+                    { email: email },
+                    process.env.JWT_SECRET,
+                    { expiresIn: "1h" }
+                );
+                return res.send({ token: token });
+            }
+            res.status(401).send({ message: "Invalid email" });
+        });
+
+        // get a admin
+        app.get("/users/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === "admin" });
+        });
+
+        
+
+        // create a user
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            // TODO: make sure you do not enter duplicate user email
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
         });
     } finally {
     }
